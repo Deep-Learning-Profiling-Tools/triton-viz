f22bc909097e01769e661597c292b1c99b278487
Sasha Rush <srush.research@gmail.com>
2024-03-05 16:35:37 -0500

    Update interface.py

--- triton_viz/interface.py
+++ triton_viz/interface.py
@@ -2,24 +2,24 @@ import gradio as gr
 import triton_viz
 import tempfile
 
-cache = {}
 
 
 def launch():
+    cache = {}
     program_records, tt = triton_viz.collect_grid()
     m = [0, 0, 0]
     for k in program_records.keys():
         m[0] = max(k[0], m[0])
         m[1] = max(k[1], m[1])
         m[2] = max(k[2], m[2])
-    print(m)
     with gr.Blocks(css=".gradio-container button {overflow: auto}") as demo:
-        with gr.Row():
-            s1 = gr.Slider(0, m[0], value=0, step=1, label="Program Id 0")
-            s2 = gr.Slider(0, m[1], value=0, step=1, label="Program Id 1")
-            s3 = gr.Slider(0, m[2], value=0, step=1, label="Program Id 2")
-        img = gr.Image(height=800, width=500)
-
+        with gr.Column():
+            img = gr.Image(height=800, width=500)
+            with gr.Row():
+                s1 = gr.Slider(0, m[0], value=0, step=1, label="Program Id 0")
+                s2 = gr.Slider(0, m[1], value=0, step=1, label="Program Id 1")
+                s3 = gr.Slider(0, m[2], value=0, step=1, label="Program Id 2")
+        
         def update(inp):
             a = inp[s1]
             b = inp[s2]
