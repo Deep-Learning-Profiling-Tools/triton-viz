5c3530e84f121f0d6070b900743403a51890bd8b
Sasha Rush <srush.research@gmail.com>
2024-03-10 17:29:55 -0400

    puzzles

--- Triton-Viz.py
+++ Triton-Viz.py
@@ -294,207 +294,69 @@ test(conv2d_kernel, conv2d_spec, B={"B0": 1, "B1": 32}, nelem={"N0": 4, "N1": 32
 
 # ## Puzzle 9: Matrix Mult
 
+# +
+@triton_viz.trace
+@triton.jit
+def dot_kernel(x_ptr, y_ptr, z_ptr, BLOCK_SIZE: tl.constexpr):
+    r = tl.program_id(0) * BLOCK_SIZE
+    c = tl.program_id(1) * BLOCK_SIZE
+    b = tl.program_id(2)
+    bid = b * 4 * BLOCK_SIZE * BLOCK_SIZE
+    x_val = tl.load(
+        x_ptr
+        + bid
+        + (r + tl.arange(0, BLOCK_SIZE)[:, None]) * 2 * BLOCK_SIZE
+        + tl.arange(0, BLOCK_SIZE)[None, :]
+    )
+    y_val = tl.load(
+        y_ptr
+        + bid
+        + tl.arange(0, BLOCK_SIZE)[:, None] * 2 * BLOCK_SIZE
+        + tl.arange(0, BLOCK_SIZE)[None, :]
+        + c
+    )
+    z = tl.dot(x_val, y_val)
+    x_val = tl.load(
+        x_ptr
+        + bid
+        + (r + tl.arange(0, BLOCK_SIZE)[:, None]) * 2 * BLOCK_SIZE
+        + tl.arange(0, BLOCK_SIZE)[None, :]
+        + BLOCK_SIZE
+    )
+    y_val = tl.load(
+        y_ptr
+        + bid
+        + (BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)[:, None]) * 2 * BLOCK_SIZE
+        + tl.arange(0, BLOCK_SIZE)[None, :]
+        + c
+    )
+    z = z + tl.dot(x_val, y_val)
+    tl.store(
+        z_ptr
+        + (b * (2 * BLOCK_SIZE) * (2 * BLOCK_SIZE - 10))
+        + (r + tl.arange(0, BLOCK_SIZE)[:, None]) * (2 * BLOCK_SIZE - 10)
+        + tl.arange(0, BLOCK_SIZE)[None, :]
+        + c,
+        z,
+        mask=tl.arange(0, BLOCK_SIZE)[None, :] + c < 2 * BLOCK_SIZE - 10,
+    )
+
+
+def perform_dot(device, BATCH, BLOCK_SIZE):
+    x = torch.randn((BATCH, 2 * BLOCK_SIZE, 2 * BLOCK_SIZE), device=device)
+    y = torch.randn((BATCH, 2 * BLOCK_SIZE, 2 * BLOCK_SIZE), device=device)
+    z = torch.zeros((BATCH, 2 * BLOCK_SIZE, 2 * BLOCK_SIZE - 10), device=device)
+    dot_kernel[(2, 2, BATCH)](x, y, z, BLOCK_SIZE)
+    return x, y, z
+BLOCK_SIZE = 32
+input_matrix1, input_matrix2, result = perform_dot(device, 12, BLOCK_SIZE)
+triton_viz.launch()
+# -
+
 # ## Puzzle 10: Quantized Matrix Mult 
+#
+# GPT-Q like puzzles
 
-# +
-@triton_viz.trace
-@triton.jit
-def dot_kernel(x_ptr, y_ptr, z_ptr, BLOCK_SIZE: tl.constexpr):
-    r = tl.program_id(0) * BLOCK_SIZE
-    c = tl.program_id(1) * BLOCK_SIZE
-    b = tl.program_id(2)
-    bid = b * 4 * BLOCK_SIZE * BLOCK_SIZE
-    x_val = tl.load(
-        x_ptr
-        + bid
-        + (r + tl.arange(0, BLOCK_SIZE)[:, None]) * 2 * BLOCK_SIZE
-        + tl.arange(0, BLOCK_SIZE)[None, :]
-    )
-    y_val = tl.load(
-        y_ptr
-        + bid
-        + tl.arange(0, BLOCK_SIZE)[:, None] * 2 * BLOCK_SIZE
-        + tl.arange(0, BLOCK_SIZE)[None, :]
-        + c
-    )
-    z = tl.dot(x_val, y_val)
-    x_val = tl.load(
-        x_ptr
-        + bid
-        + (r + tl.arange(0, BLOCK_SIZE)[:, None]) * 2 * BLOCK_SIZE
-        + tl.arange(0, BLOCK_SIZE)[None, :]
-        + BLOCK_SIZE
-    )
-    y_val = tl.load(
-        y_ptr
-        + bid
-        + (BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)[:, None]) * 2 * BLOCK_SIZE
-        + tl.arange(0, BLOCK_SIZE)[None, :]
-        + c
-    )
-    z = z + tl.dot(x_val, y_val)
-    tl.store(
-        z_ptr
-        + (b * (2 * BLOCK_SIZE) * (2 * BLOCK_SIZE - 10))
-        + (r + tl.arange(0, BLOCK_SIZE)[:, None]) * (2 * BLOCK_SIZE - 10)
-        + tl.arange(0, BLOCK_SIZE)[None, :]
-        + c,
-        z,
-        mask=tl.arange(0, BLOCK_SIZE)[None, :] + c < 2 * BLOCK_SIZE - 10,
-    )
-
-
-def perform_dot(device, BATCH, BLOCK_SIZE):
-    x = torch.randn((BATCH, 2 * BLOCK_SIZE, 2 * BLOCK_SIZE), device=device)
-    y = torch.randn((BATCH, 2 * BLOCK_SIZE, 2 * BLOCK_SIZE), device=device)
-    z = torch.zeros((BATCH, 2 * BLOCK_SIZE, 2 * BLOCK_SIZE - 10), device=device)
-    dot_kernel[(2, 2, BATCH)](x, y, z, BLOCK_SIZE)
-    return x, y, z
-BLOCK_SIZE = 32
-input_matrix1, input_matrix2, result = perform_dot(device, 12, BLOCK_SIZE)
-triton_viz.launch()
-
-
-# -
-
-@triton_viz.trace
-@triton.jit
-def add_kernel(X, Y, Z, n_elements,  # Size of the vector.  BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
-    # NOTE: `constexpr` so it can be used as a shape value.
-):
-    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
-    # This program will process inputs that are offset from the initial data.
-    # For instance, if you had a vector of length 256 and block_size of 64, the programs
-    # would each access the elements [0:64, 64:128, 128:192, 192:256].
-    # Note that offsets is a list of pointers:
-    block_start = pid * BLOCK_SIZE
-    offsets = block_start + tl.arange(0, BLOCK_SIZE)
-    # Create a mask to guard memory operations against out-of-bounds accesses.
-    mask = offsets < n_elements
-    # Load x and y from DRAM, masking out any extra elements in case the input is not a
-    # multiple of the block size.
-    x = tl.load(x_ptr + offsets, mask=mask)
-    y = tl.load(y_ptr + offsets, mask=mask)
-    output = x + y
-    # Write x + y back to DRAM.
-    tl.store(output_ptr + offsets, output, mask=mask)
-
-
-
-# ## Puzzle 2: Blocking
-
-# +
-@triton_viz.trace
-@triton.jit
-def add_kernel(
-    x_ptr,  # *Pointer* to first input vector.
-    y_ptr,  # *Pointer* to second input vector.
-    output_ptr,  # *Pointer* to output vector.
-    n_elements,  # Size of the vector.
-    BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
-    # NOTE: `constexpr` so it can be used as a shape value.
-):
-    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
-    # This program will process inputs that are offset from the initial data.
-    # For instance, if you had a vector of length 256 and block_size of 64, the programs
-    # would each access the elements [0:64, 64:128, 128:192, 192:256].
-    # Note that offsets is a list of pointers:
-    block_start = pid * BLOCK_SIZE
-    offsets = block_start + tl.arange(0, BLOCK_SIZE)
-    # Create a mask to guard memory operations against out-of-bounds accesses.
-    mask = offsets < n_elements
-    # Load x and y from DRAM, masking out any extra elements in case the input is not a
-    # multiple of the block size.
-    x = tl.load(x_ptr + offsets, mask=mask)
-    y = tl.load(y_ptr + offsets, mask=mask)
-    output = x + y
-    # Write x + y back to DRAM.
-    tl.store(output_ptr + offsets, output, mask=mask)
-
-
-def add(x: torch.Tensor, y: torch.Tensor):
-    # We need to preallocate the output.
-    output = torch.empty_like(x)
-    n_elements = output.numel()
-    # The SPMD launch grid denotes the number of kernel instances that run in parallel.
-    # It is analogous to CUDA launch grids. It can be either Tuple[int], or Callable(metaparameters) -> Tuple[int].
-    # In this case, we use a 1D grid where the size is the number of blocks:
-    grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
-    # NOTE:
-    #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
-    #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
-    #  - Don't forget to pass meta-parameters as keywords arguments.
-    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
-    # We return a handle to z but, since `torch.cuda.synchronize()` hasn't been called, the kernel is still
-    # running asynchronously at this point.
-    return output, grid
-
-    # Directly use x and y here even though they are defined later in the file
-
-
-
-# -
-
-device = "cpu"
-size = 5000
-input_vector1, input_vector2, output_triton = perform_vec_add(device, size)
-triton_viz.launch()
-
-
-# +
-@triton_viz.trace
-@triton.jit
-def dot_kernel(x_ptr, y_ptr, z_ptr, BLOCK_SIZE: tl.constexpr):
-    r = tl.program_id(0) * BLOCK_SIZE
-    c = tl.program_id(1) * BLOCK_SIZE
-    b = tl.program_id(2)
-    bid = b * 4 * BLOCK_SIZE * BLOCK_SIZE
-    x_val = tl.load(
-        x_ptr
-        + bid
-        + (r + tl.arange(0, BLOCK_SIZE)[:, None]) * 2 * BLOCK_SIZE
-        + tl.arange(0, BLOCK_SIZE)[None, :]
-    )
-    y_val = tl.load(
-        y_ptr
-        + bid
-        + tl.arange(0, BLOCK_SIZE)[:, None] * 2 * BLOCK_SIZE
-        + tl.arange(0, BLOCK_SIZE)[None, :]
-        + c
-    )
-    z = tl.dot(x_val, y_val)
-    x_val = tl.load(
-        x_ptr
-        + bid
-        + (r + tl.arange(0, BLOCK_SIZE)[:, None]) * 2 * BLOCK_SIZE
-        + tl.arange(0, BLOCK_SIZE)[None, :]
-        + BLOCK_SIZE
-    )
-    y_val = tl.load(
-        y_ptr
-        + bid
-        + (BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)[:, None]) * 2 * BLOCK_SIZE
-        + tl.arange(0, BLOCK_SIZE)[None, :]
-        + c
-    )
-    z = z + tl.dot(x_val, y_val)
-    tl.store(
-        z_ptr
-        + (b * (2 * BLOCK_SIZE) * (2 * BLOCK_SIZE - 10))
-        + (r + tl.arange(0, BLOCK_SIZE)[:, None]) * (2 * BLOCK_SIZE - 10)
-        + tl.arange(0, BLOCK_SIZE)[None, :]
-        + c,
-        z,
-        mask=tl.arange(0, BLOCK_SIZE)[None, :] + c < 2 * BLOCK_SIZE - 10,
-    )
-
-
-def perform_dot(device, BATCH, BLOCK_SIZE):
-    x = torch.randn((BATCH, 2 * BLOCK_SIZE, 2 * BLOCK_SIZE), device=device)
-    y = torch.randn((BATCH, 2 * BLOCK_SIZE, 2 * BLOCK_SIZE), device=device)
-    z = torch.zeros((BATCH, 2 * BLOCK_SIZE, 2 * BLOCK_SIZE - 10), device=device)
-    dot_kernel[(2, 2, BATCH)](x, y, z, BLOCK_SIZE)
-    return x, y, z
-BLOCK_SIZE = 32
-input_matrix1, input_matrix2, result = perform_dot(device, 12, BLOCK_SIZE)
-triton_viz.launch()
+# ## Puzzle 11: Flash Attention 
+#
+# Long reduction. 
