18a8de83a816bf4bba903e44e3405d0a468dea95
Hao Wu <hwu27@gmu.edu>
2024-11-12 12:36:10 -0500

    set progress report as a feature

--- triton_viz/core/config.py
+++ triton_viz/core/config.py
@@ -3,6 +3,7 @@ import os
 
 # configs
 trace_enabled = os.getenv('ENABLE_TRITON_SANITIZER', '0') == '1'
+report_grid_execution_progress = os.getenv('REPORT_GRID_EXECUTION_PROGRESS', '0') == '1'
 
 # global states
 trace_warning_toggled = False
