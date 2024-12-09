17b1b8828d67996f58df93c4591b094a7ed00bbd
Hao Wu <hwu27@gmu.edu>
2024-12-09 13:41:09 -0500

    [DEV] Sync with main (#53)
    Co-authored-by: Jokeren <robinho364@gmail.com>

--- setup.py
+++ setup.py
@@ -1,20 +1,4 @@
-from setuptools import setup, find_packages
+# setup.py
+from setuptools import setup
 
-setup(
-    name="triton-viz",
-    version="2.0",
-    packages=find_packages(),
-    include_package_data=True,
-    description="A visualization tool for Triton",
-    author="Deep Learning Profiling Tools Team",
-    author_email="kzhou6@gmu.edu",
-    url="https://github.com/Deep-Learning-Profiling-Tools/triton-viz",
-    install_requires=[
-        "setuptools",
-        "triton",
-        "gradio",
-        "pyarrow",
-        "pre-commit",
-        "pytest",
-    ],
-)
+setup()
