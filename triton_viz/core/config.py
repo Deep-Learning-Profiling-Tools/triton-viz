ecd58a012ef649f69c4b531710607aab03380354
Hao Wu <hwu27@gmu.edu>
2024-12-12 12:48:03 -0500

    [DEV] Refactor global variables, environment variables, get_tensor and get_traceback_info (#57)
    Co-authored-by: Keren Zhou <robinho364@gmail.com>

--- triton_viz/core/config.py
+++ triton_viz/core/config.py
@@ -2,13 +2,14 @@ import os
 
 
 # global configs
-trace_enabled = os.getenv('ENABLE_TRITON_SANITIZER', '0') == '1'
 report_grid_execution_progress = os.getenv('REPORT_GRID_EXECUTION_PROGRESS', '0') == '1'
 
 # sanitizer configs
 sanitizer_backend = os.getenv("TRITON_SANITIZER_BACKEND", "")
+available_backends = ["off", "brute_force", "z3"]
+if sanitizer_backend == "":
+    print(f"TRITON_SANITIZER_BACKEND not set. Available backends are: {available_backends}. Defaulting to 'off'.")
+    sanitizer_backend = "off"
+elif sanitizer_backend not in available_backends:
+    raise ValueError(f"Invalid TRITON_SANITIZER_BACKEND: {sanitizer_backend}. Available backends are: {available_backends}")
 
-# global states
-global_warning_toggled = dict()
-global_warning_toggled['sanitizer'] = False
-global_warning_toggled['trace'] = False
