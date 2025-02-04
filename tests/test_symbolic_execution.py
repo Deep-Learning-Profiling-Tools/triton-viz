9419c75041d52c832b650734fb64adc969ecf0db
Hao Wu <hwu27@gmu.edu>
2025-02-03 22:23:51 -0500

    add test_vec_add for symexec

--- tests/test_symbolic_execution.py
+++ tests/test_symbolic_execution.py
@@ -13,7 +13,7 @@ def test_program_id():
     a = torch.randn(16, dtype=torch.float32, device='cuda')
     program_id_kernel[(16,)](a)
 
-def test_tl_add():
+def test_tl_addptr():
     @triton_viz.trace(clients=Sanitizer(abort_on_error=True))
     @triton.jit
     def add_kernel(x):
@@ -22,3 +22,27 @@ def test_tl_add():
         tl.load(addr)
     a = torch.randn(16, dtype=torch.float32, device='cuda')
     add_kernel[(16,)](a)
+
+def test_vec_add():
+    @triton_viz.trace(clients=Sanitizer(abort_on_error=True))
+    @triton.jit
+    def add_kernel(x_ptr, y_ptr, output_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
+        pid = tl.program_id(0)
+        block_start = pid * BLOCK_SIZE
+        offsets = block_start + tl.arange(0, BLOCK_SIZE)
+        mask = offsets < n_elements
+        x = tl.load(x_ptr + offsets, mask=mask)
+        y = tl.load(y_ptr + offsets, mask=mask)
+        output = tl.zeros(x.shape, dtype=x.dtype) + x + y
+        tl.store(output_ptr + offsets, output, mask=mask)
+
+    access_size = 16
+    size = 16
+    BLOCK_SIZE = 8
+    a = torch.randn(size, dtype=torch.float32, device='cuda')
+    b = torch.randn(size, dtype=torch.float32, device='cuda')
+
+    output = torch.empty_like(a, device='cuda')
+
+    grid = lambda meta: (triton.cdiv(access_size, meta["BLOCK_SIZE"]),)
+    add_kernel[grid](a, b, output, access_size, BLOCK_SIZE=BLOCK_SIZE)
