7a8a2ff733049d2586fe3bac51145e334cfb46fa
Hao Wu <hwu27@gmu.edu>
2025-06-23 20:00:38 -0400

    tests for sanitizer's init functions

--- tests/symbolic_execution/test_init.py
+++ tests/symbolic_execution/test_init.py
@@ -1,5 +1,10 @@
+from triton_viz.core.client import Client
 from triton_viz.clients import Sanitizer
-from triton_viz.clients.sanitizer.sanitizer import SanitizerBruteForce, NullSanitizer, SanitizerSymbolicExecution
+from triton_viz.clients.sanitizer.sanitizer import (
+    SanitizerBruteForce,
+    NullSanitizer,
+    SanitizerSymbolicExecution,
+)
 from triton_viz import config as cfg
 
 
@@ -8,12 +13,19 @@ def test_brute_force():
     s1 = Sanitizer(abort_on_error=True)
     assert isinstance(s1, SanitizerBruteForce) and s1.abort_on_error is True
 
+
 def test_null_sanitizer():
     cfg.sanitizer_backend = "off"
     s2 = Sanitizer(abort_on_error=True)
     assert isinstance(s2, NullSanitizer)
 
+
 def test_symbolic_execution():
     cfg.sanitizer_backend = "symexec"
     s3 = Sanitizer(abort_on_error=True)
     assert isinstance(s3, SanitizerSymbolicExecution) and s3.abort_on_error is True
+
+
+def test_default_sanitizer():
+    s = Sanitizer()
+    assert isinstance(s, Client)
