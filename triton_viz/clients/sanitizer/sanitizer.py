c4ceedf9e6d01d04ce2883946225d8cdee1a93bc
Jokeren <robinho364@gmail.com>
2025-08-12 14:23:56 -0500

    Update

--- triton_viz/clients/sanitizer/sanitizer.py
+++ triton_viz/clients/sanitizer/sanitizer.py
@@ -905,7 +905,7 @@ class SymbolicExpr:
             self._constraints.extend(constraints_ptr)
             if self.mask is not None:
                 mask, _ = self.mask._to_z3()
-                self._constraints.extend(mask)
+                self._constraints.append(mask)
             self._z3 = ptr
 
         if self.op in ("splat", "expand_dims", "broadcast"):
