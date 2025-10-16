5adb5e035e6c4c797193d95a339a316a642937bf
Thai-Hoa Nguyen <tonanhhoaduc@gmail.com>
2025-10-15 21:08:58 -0400

    hardcode offsets and callbacks for NKI matmul kernel

--- nki-examples/matmul.py
+++ nki-examples/matmul.py
@@ -11,6 +11,8 @@ from triton_viz.core.trace import launches
 import numpy as np
 import math
 
+tensors = [] # THTODO: rm
+records = [triton_viz.core.data.Grid((0,0,0))] # THTODO: rm
 def matmul_kernel(lhs, rhs):
     """NKI matmul_kernel to compute a matrix multiplication operation in a tiled manner
 
@@ -34,6 +36,9 @@ def matmul_kernel(lhs, rhs):
     TILE_K = 2
     TILE_N = 4
 
+    tensors.append(lhs)
+    tensors.append(rhs)
+    tensors.append(result)
     # Use affine_range to loop over tiles
     m_range = nl.arange(M)[:, None]
     n_range = nl.arange(N)[None, :]
@@ -50,62 +55,89 @@ def matmul_kernel(lhs, rhs):
                 # Load tiles from lhs and rhs
                 lhs_p = nl.arange(TILE_M)[:, None] + m * TILE_M
                 lhs_f = nl.arange(TILE_K)[None, :] + k * TILE_K
-                lhs_tile = nl.load(lhs[lhs_p, lhs_f], mask=(lhs_p < M) & (lhs_f < K))
+                lhs_mask = (lhs_p < M) & (lhs_f < K)
+                lhs_tile = nl.load(lhs[lhs_p, lhs_f], mask=lhs_mask)
+                records.append(triton_viz.core.data.Load(
+                    lhs.data_ptr(),
+                    (4 * (lhs_p * K + lhs_f))._value,
+                    lhs_mask._value
+                )) # THTODO: rm
+
                 rhs_p = nl.arange(TILE_K)[:, None] + k * TILE_K
                 rhs_f = nl.arange(TILE_N)[None, :] + n * TILE_N
-                rhs_tile = nl.load(rhs[rhs_p, rhs_f], mask=(rhs_p < K) & (rhs_f < N))
+                rhs_mask = (rhs_p < K) & (rhs_f < N) 
+                rhs_tile = nl.load(rhs[rhs_p, rhs_f], mask=rhs_mask)
+                records.append(triton_viz.core.data.Load(
+                    rhs.data_ptr(),
+                    (4 * (rhs_p * N + rhs_f))._value,
+                    rhs_mask._value
+                )) # THTODO: rm
 
                 # Accumulate partial-sums into PSUM
-                res_psum += nl.matmul(lhs_tile[...], rhs_tile[...], transpose_x=False)
+                x = nl.matmul(lhs_tile[...], rhs_tile[...], transpose_x=False)
+                res_psum += x
+                records.append(triton_viz.core.data.Dot(
+                    lhs_tile.shape,
+                    rhs_tile.shape,
+                    res_psum.shape,
+                    lhs_tile._value,
+                    rhs_tile._value,
+                    x._value
+                )) # THTODO: rm
 
             # Copy the result from PSUM back to SBUF, and cast to expected output data-type
-            out_mask = (m * TILE_M <= m_range) & (m_range < (m + 1) * TILE_M)
-            out_mask &= (n * TILE_N <= n_range) & (n_range < (n + 1) * TILE_N)
             res_sb = nl.copy(res_psum, dtype=result.dtype)
 
-            #nl.store(result[m * TILE_M:(m + 1) * TILE_M, n * TILE_N:(n + 1) * TILE_N],
-            #         value=res_sb) # TODO: uncomment this
-            nl.store(result, value=res_sb, mask=out_mask) # TODO: remove this - incorrect NKI syntax but currently needed for interpreter
+            out_p = nl.arange(TILE_M)[:, None] + m * TILE_M
+            out_f = nl.arange(TILE_N)[None, :] + n * TILE_N
+            out_mask = (out_p < M) & (out_f < N)
+            nl.store(result[m * TILE_M:(m + 1) * TILE_M, n * TILE_N:(n + 1) * TILE_N],
+                     value=res_sb)
+            records.append(triton_viz.core.data.Store(
+                result.data_ptr(),
+                (4 * (out_p * N + out_f))._value,
+                out_mask._value
+            )) # THTODO: rm
 
     return result
 
 TRITON_VIZ = True
 kernel_grid = (1, 1, 1)
-lhs_small = np.arange(64).astype(np.float32).reshape(8, 8)
-rhs_small = np.arange(128).astype(np.float32).reshape(8, 16)
-#lhs_small = np.arange(16).astype(np.float32).reshape(4, 4)
-#rhs_small = np.arange(32).astype(np.float32).reshape(4, 8)
+lhs_small = np.arange(16).astype(np.float32).reshape(4, 4)
+rhs_small = np.arange(32).astype(np.float32).reshape(4, 8)
 kernel_args = (lhs_small, rhs_small)
 
 if TRITON_VIZ:
-    tp.log('Executing matmul_kernel with NKI interpreter...')
+    print('Executing matmul_kernel with NKI interpreter...')
     matmul_kernel = triton_viz.trace(clients=Tracer(), backend="nki")(matmul_kernel)
     kk = matmul_kernel[kernel_grid]
     z2 = kk(*kernel_args)
 
-    #print(f"Number of launches: {len(launches)}")
-    #if launches:
-    #    launch = launches[-1]
-    #    print(f"Number of records: {len(launch.records)}")
-    #    for i, record in enumerate(launch.records):
-    #        print(f"Record {i}: {type(record).__name__}")
-    #        if hasattr(record, "ptr"):
-    #            print(f"  ptr: {record.ptr}")
-    #        if hasattr(record, "offsets"):
-    #            print(f"  offsets shape: {record.offsets.shape}")
-    #        if hasattr(record, "masks"):
-    #            print(f"  masks shape: {record.masks.shape}")
+    print(f"Number of launches: {len(launches)}")
+    if launches:
+        launch = launches[-1]
+        launch.records = records # THTODO: rm
+        launch.tensors = tensors # THTODO: rm
+        print(f"Number of records: {len(launch.records)}")
+        for i, record in enumerate(launch.records):
+            print(f"Record {i}: {type(record).__name__}")
+            if hasattr(record, "ptr"):
+                print(f"  ptr: {record.ptr}")
+            if hasattr(record, "offsets"):
+                print(f"  offsets shape: {record.offsets.shape}")
+            if hasattr(record, "masks"):
+                print(f"  masks shape: {record.masks.shape}")
 
-    ## Try to launch visualization
-    #try:
-    #    triton_viz.launch(share=False)
-    #except Exception as e:
-    #    print(f"\nError during visualization: {e}")
-    #    import traceback
+    # Try to launch visualization
+    try:
+        triton_viz.launch(share=False)
+    except Exception as e:
+        print(f"\nError during visualization: {e}")
+        import traceback
 
-    #    traceback.print_exc()
+        traceback.print_exc()
 else:
-    tp.log('Executing NKI JIT-ed matmul_kernel...')
+    print('Executing NKI JIT-ed matmul_kernel...')
     matmul_kernel = nki.jit(matmul_kernel)
     z2 = nki.simulate_kernel(matmul_kernel[kernel_grid], *kernel_args)
 
