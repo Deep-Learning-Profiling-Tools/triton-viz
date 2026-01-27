f1dc7959613591a7227c852b53b454ba7dafc9a3
Thai-Hoa Nguyen <tonanhhoaduc@gmail.com>
2026-01-27 09:07:27 -0500

    remove stuff

--- triton_viz/static/ops/defaults.js
+++
@@ -1,7 +0,0 @@
-import { registerVisualizer } from './registry.js';
-import { createMatMulVisualization } from './matmul.js';
-import { createLoadVisualization } from './load.js';
-import { createStoreVisualization } from './store.js';
-registerVisualizer('Dot', createMatMulVisualization);
-registerVisualizer('Load', createLoadVisualization);
-registerVisualizer('Store', createStoreVisualization);
