3d6742be30389d997af52b36d3c59cc51595ebb2
Hao Wu <hwu27@gmu.edu>
2026-02-12 17:38:54 -0500

    Remove tritonbench_kernels folder and associated tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,4 @@
-import os
-
 import pytest
-
-# tritonbench_kernels/ contains standalone scripts run via triton-sanitizer,
-# not test modules. Exclude them from pytest collection.
-collect_ignore_glob = [
-    os.path.join(os.path.dirname(__file__), "end_to_end", "tritonbench_kernels", "*"),
-]
 
 
 @pytest.fixture(scope="session", params=["cpu"])
