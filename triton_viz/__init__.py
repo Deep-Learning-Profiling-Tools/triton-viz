15448dd017e91cbe5aeedf49484fb7abde92106f
Thai-Hoa Nguyen <tonanhhoaduc@gmail.com>
2026-02-14 18:04:07 -0500

    init

--- triton_viz/__init__.py
+++ triton_viz/__init__.py
@@ -1,6 +1,14 @@
 from .core import trace, clear
 from .core.config import config
 from .version import __version__, git_version
-from .visualizer import launch
+from .visualizer import launch, stop_server
 
-__all__ = ["trace", "clear", "config", "launch", "__version__", "git_version"]
+__all__ = [
+    "trace",
+    "clear",
+    "config",
+    "launch",
+    "__version__",
+    "git_version",
+    "stop_server",
+]
--- triton_viz/visualizer/__init__.py
+++ triton_viz/visualizer/__init__.py
@@ -1,4 +1,4 @@
 from .draw import collect_grid
-from .interface import launch
+from .interface import launch, stop_server
 
-__all__ = ["collect_grid", "launch"]
+__all__ = ["collect_grid", "launch", "stop_server"]
