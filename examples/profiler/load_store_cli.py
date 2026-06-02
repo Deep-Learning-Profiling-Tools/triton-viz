9024d66719473302969953cb09a075c3aa1a24d3
Keren Zhou <robinho364@gmail.com>
2026-06-02 10:08:45 -0400

    [DOCS] Simplify README usage guide (#378)

---
+++ examples/profiler/load_store_cli.py
@@ -0,0 +1,29 @@
+import torch
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def simple_kernel(
+    x_ptr,
+    output_ptr,
+    n_elements,
+    BLOCK_SIZE: tl.constexpr,
+):
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    x = tl.load(x_ptr + offsets, mask=mask)
+    tl.store(output_ptr + offsets, x, mask=mask)
+
+
+if __name__ == "__main__":
+    device = "cpu"
+    size = 12
+    BLOCK_SIZE = 8
+    torch.manual_seed(0)
+    x = torch.arange(size, dtype=torch.float32, device=device)
+    output = torch.empty_like(x)
+    grid = lambda meta: (triton.cdiv(size, meta["BLOCK_SIZE"]),)
+    simple_kernel[grid](x, output, size, BLOCK_SIZE)
