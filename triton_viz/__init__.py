8f544a59ccac19e244bf7e5b162570b051ea5b64
Pham D Hung <hungduypham2004@gmail.com>
2026-09-16 16:09:06 -0400

    [FIX] Keep triton_viz imports working

---
+++ triton_viz/__init__.py
@@ -0,0 +1,13 @@
+"""Backward compatability with triton_viz"""
+
+from tilelens import (
+    __all__ as __all__,
+    __version__ as __version__,
+    clear as clear,
+    config as config,
+    git_version as git_version,
+    launch as launch,
+    load as load,
+    save as save,
+    trace as trace,
+)
